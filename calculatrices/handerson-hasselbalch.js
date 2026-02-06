2b2f73efe17fef7a1efafe2e6e45c89500517879
Nicolas Blais St-Laurent <nicolas.blais.stlaurent@gmail.com>
2026-02-06 06:07:47 -0500

    Built site for gh-pages

--- calculatrices/handerson-hasselbalch.js
+++ calculatrices/handerson-hasselbalch.js
@@ -1,14 +1,14 @@
-export function HH_pH(PCO2, HCO3=24, pKa=6.1, alpha=0.03){
+export function pH(PCO2, HCO3=24, pKa=6.1, alpha=0.03){
   let pH = pKa + Math.log10(HCO3/(alpha * PCO2))
   return pH
 }
 
-export function HH_PCO2(pH, HCO3, pKa=6.1, alpha=0.03){
+export function PCO2(pH, HCO3, pKa=6.1, alpha=0.03){
   let pCO2 = HCO3 / (10**(pH-pKa) * alpha)
   return pCO2
 }
 
-export function HH_HCO3(pH, PCO2, pKa=6.1, alpha=0.03){
+export function HCO3(pH, PCO2, pKa=6.1, alpha=0.03){
   let HCO3 = PCO2 * 10**(pH-pKa) * alpha
   return HCO3
 }
